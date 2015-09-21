157b2484914bdd97f1ab07fdf063ce07f2e9a14b
Maroof Khan <maroof-khan@live.com>
2015-09-21 08:09:15 -0700

    initial-setup.

---
+++ application/routes.js
@@ -0,0 +1,12 @@
+
+module.exports = function (application) {
+    application.get ('/api',
+                    function (request, response) {
+                        response.json(
+                            {
+                                message: "Central Hub!"
+                            }
+                        );
+                    }
+    )
+}
