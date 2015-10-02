1b122d979b4a7785f8d30e5648adee5d41ba39e4
furqan-mk9 <furqan.mkhan@hotmail.com>
2015-10-02 12:37:22 -0700

    A little more work

--- application/routes.js
+++ application/routes.js
@@ -1,14 +1,24 @@
 
 module.exports = function (application) {
-    application.get('/api', 
+    application.get ('/api', 
         function (request, response) {
             response.json({
                 message: "Central Hub!"
             });
         });
                     
-    application.post('/api/',
-        function() {
-                
+    application.post ('/api/trypost',
+        function (request, response) {
+            
+            /*
+            response.on("data", function(){})
+            
+            response.on("end", function() {
+                console.log("Status Code: " + response.statusCode);
+            });
+            */
+            var message = request.body.message;
+            console.log(message);
+            response.send(message);
         });
 }
